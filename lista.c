0c0f6258f37014a453fa4b3df844dacdca342767
Pedro Vargas <pedrovargastannuri@gmail.com>
2022-07-07 11:54:19 -0300

    Implementacao do strtok na mao

--- lista.c
+++ lista.c
@@ -18,21 +18,21 @@ int importaTexto(struct descritor *desc, FILE *fp)
 
     desc->primeiraLinha = malloc(sizeof(struct noLinha));
 
-    if(desc->primeiraLinha == NULL)
+    if (desc->primeiraLinha == NULL)
         return -1;
 
     char linha[900];
 
     struct noLinha *aux = malloc(sizeof(struct noLinha));
 
-    while (fgets(linha, 900, fp) != NULL) //While da linha
+    while (fgets(linha, 900, fp) != NULL) // While da linha
     {
-        char *palavra;
         numLinha += 1;
         int numCol = 1;
-
+        int quantEspacos = 0;
+        int acabou = 0;
         struct noLinha *temp = malloc(sizeof(struct noLinha));
-        if(temp == NULL)
+        if (temp == NULL)
         {
             return -1;
         }
@@ -40,17 +40,53 @@ int importaTexto(struct descritor *desc, FILE *fp)
         temp->primeiraPalavra = malloc(sizeof(struct noPalavra));
         struct noPalavra *aux2 = malloc(sizeof(struct noPalavra));
 
-        palavra = strtok(linha, ".!,? ");
-        while (palavra)
+        for (int i = 0; i < strlen(linha); i += 1)
+        {
+            char *palavra;
+            if (linha[i] != " " && acabou == 0)
+            {
+                palavra += linha[i];
+            }
+            else if (linha[i] == " ")
+            {
+                quantEspacos += 1;
+                acabou = 1;
+            }
+            else
+            {
+                struct noPalavra *temp2 = malloc(sizeof(struct noPalavra));
+                if (temp2 == NULL)
+                {
+                    return -1;
+                }
+                strcpy(temp2->palavra, palavra);
+                temp2->col = numCol;
+                if (numCol == 1)
+                {
+                    temp2->antPal = NULL;
+                    temp->primeiraPalavra = temp2;
+                }
+                else
+                {
+                    temp2->antPal = aux2;
+                }
+                aux2->proxPal = temp2;
+                temp2->proxPal = NULL;
+                aux2 = temp2;
+                numCol += strlen(palavra) + quantEspacos;
+                quantEspacos = 0;
+            }
+        }
+        /*while (palavra)
         {
             struct noPalavra *temp2 = malloc(sizeof(struct noPalavra));
-            if(temp2 == NULL)
+            if (temp2 == NULL)
             {
                 return -1;
             }
             strcpy(temp2->palavra, palavra);
             temp2->col = numCol;
-            if(numCol == 1)
+            if (numCol == 1)
             {
                 temp2->antPal = NULL;
                 temp->primeiraPalavra = temp2;
@@ -64,8 +100,8 @@ int importaTexto(struct descritor *desc, FILE *fp)
             aux2 = temp2;
             numCol += strlen(palavra) + 1;
             palavra = strtok(NULL, ".!,? ");
-        }
-        if(numLinha == 1)
+        }*/
+        if (numLinha == 1)
         {
             temp->antLin = NULL;
             desc->primeiraLinha = temp;
