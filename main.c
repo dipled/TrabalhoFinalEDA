e161e69c59dea1a34797dc842939bffda7364989
gustavokf2003 <66067122+gustavokf2003@users.noreply.github.com>
2022-07-08 16:44:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,23 +14,6 @@ int main()
     }
     importaTexto(desc, fp);
     exibeTexto(desc);
-    printf("Numero total da palavra Teste: %d\n",numTotalCertaPalavra(desc, "Teste"));
-
-    int *ocorrencias1 = buscaPalavra(desc, "teste");
-    if(ocorrencias1 != NULL){
-        for(int i = 0; i < ocorrencias1[0]*2; i += 2){
-            printf("(%d,%d)\n",ocorrencias1[i+1],ocorrencias1[i+2]);
-        }
-    }
-
-    int *ocorrencias = numSubPalavra(desc, "tes");
-    if(ocorrencias != NULL){
-        printf("Numero de ocorrencia da subString: %d\n", ocorrencias[0]);
-        for(int i = 0; i < ocorrencias[0]*2; i += 2){
-            printf("(%d,%d)\n",ocorrencias[i+1],ocorrencias[i+2]);
-        }
-    }
-    printf("Numero de palavras: %d\n",numTotalPalavra(desc));
 
     insercao(desc, "alo", 16, 3);
     exibeTexto(desc);
